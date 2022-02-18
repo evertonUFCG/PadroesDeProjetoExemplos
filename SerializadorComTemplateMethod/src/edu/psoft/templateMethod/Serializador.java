5751c1c8d67bb377cf8478c791144fce2c14e8e7
evertonUFCG <39099565+evertonUFCG@users.noreply.github.com>
2022-02-18 15:38:52 -0300

    Add files via upload

--- SerializadorComTemplateMethod/src/edu/psoft/templateMethod/Serializador.java
+++ SerializadorComTemplateMethod/src/edu/psoft/templateMethod/Serializador.java
@@ -4,51 +4,25 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Map;
 
-public class Serializador {
-	public static int TXT = 1;
-	public static int XML = 2;
+public abstract class Serializador {
 	
-	public void gerarArquivo(int opcaoGeracao, String nome, 
+	public void gerarArquivo(String nome, 
 			Map<String,Object> propriedades)
 					throws IOException{
 
-		String conteudo = "";
-		if (opcaoGeracao == TXT)
-			conteudo = gerarConteudoTXT(propriedades);
-		else if (opcaoGeracao == XML)
-			conteudo = gerarConteudoXML(propriedades);
+		String conteudo = gerarConteudo(propriedades);
 
 		byte [] bytes = conteudo.getBytes();
 
-		if (opcaoGeracao == TXT)
-			//transforma bytes em
-			bytes = criptografar(bytes);
-		else if (opcaoGeracao == XML)
-			bytes = compactar(bytes);
+		bytes = posprocessar(bytes);
 
 		FileOutputStream fileout = new FileOutputStream(nome);
 		fileout.write(bytes);
 		fileout.close();
 	}
-
-	private String gerarConteudoXML(Map<String, Object> propriedades) {
-		System.out.println("Criando conteudo XML");
-		return "Conteudo XML";
-	}
-
-	private String gerarConteudoTXT(Map<String, Object> propriedades) {
-		System.out.println("Criando conteudo TXT");
-		return "Conteudo TXT";
-	}
 	
-	private byte[] compactar(byte[] bytes) {
-		System.out.println("Compactando...");
-		return new byte[1];
-	}
-
-	private byte[] criptografar(byte[] bytes) {
-		System.out.println("Criptografando...");
-		return new byte[1];
-	}
+	public abstract String gerarConteudo(Map<String, Object> propriedades);
+	
+	public abstract byte[] posprocessar(byte[] bytes);
 
 }
--- SerializadorComTemplateMethod/src/edu/psoft/templateMethod/test/Client.java
+++ SerializadorComTemplateMethod/src/edu/psoft/templateMethod/test/Client.java
@@ -3,19 +3,19 @@ package edu.psoft.templateMethod.test;
 import java.io.IOException;
 import java.util.HashMap;
 
-import edu.psoft.templateMethod.Serializador;
+import edu.psoft.templateMethod.*;
 
 class Client {
 
 	public static void main(String args[]) {
-//		Serializador serializador = new SerializadorTXTCompactado();
-//		Serializador serializador2 = new SerializadorXMLCriptografado();
-//		HashMap<String, Object> properties = new HashMap<String,Object>();
-//		properties.put("p1", null);
-//		try {
-//			serializador.gerarArquivo("propriedades", properties);
-//			serializador2.gerarArquivo("propriedades", properties);
-//		} catch (IOException e) {
-//		}
+		Serializador serializador = new SerializadorTXTCompactado();
+		Serializador serializador2 = new SerializadorXMLCriptografado();
+		HashMap<String, Object> properties = new HashMap<String,Object>();
+		properties.put("p1", null);
+		try {
+			serializador.gerarArquivo("propriedades", properties);
+			serializador2.gerarArquivo("propriedades", properties);
+		} catch (IOException e) {
+		}
 	}
 }
